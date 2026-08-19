fb2df12aae870f57c43db2d18270ee3779c13814
Rebeca <rebecadmsilva@gmail.com>
2026-08-19 16:38:28 -0300

    feat: bloqueia drop de cadeiras especificas

--- src/components/ColunaPeriodo.jsx
+++ src/components/ColunaPeriodo.jsx
@@ -21,7 +21,7 @@ export function ColunaPeriodo({
 
       <div className="header-periodo-top">
         <h2 className="titulo-periodo">{numeroPeriodo}° Período</h2>
-        <span className="contagem-disciplinas">{totalCadeiras} cad</span>
+        <span className="contagem-cadeiras">{totalCadeiras} cad</span>
       </div>
 
       <div
